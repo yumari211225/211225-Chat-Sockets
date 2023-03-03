d9b85c235a3f4c596f34d8d13b09bbc6deaaa1c6
yumari211225 <211225@ids.upchiapas.edu.mx>
2023-03-02 21:37:49 -0600

    Chat con WebSockets

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app)
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000
+ 
+const usuarios = {};
+
+server.listen(PORT, () => {
+   console.log('Servidor ejecutando en puerto: ' + PORT);
+});
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+io.on('connection', (socket) => {
+
+   socket.on('register', ( username ) => {
+      if ( usuarios[username] ) {
+         socket.emit('login-issue');
+         return;
+      } else {
+         usuarios[username] = socket.id;
+         socket.username = username;
+         socket.emit('login');
+         io.emit('user-connected', usuarios);
+      }
+   });
+
+   socket.on('send-message', ({message, image}) => {
+      io.emit('send-message', {message, user: socket.username, image});
+   });
+
+   socket.on('send-private-message', ({targetUser, message, image}) => {
+      if ( usuarios[targetUser] ) {
+         io.to(usuarios[targetUser]).emit('send-private-message', { from: socket.username, message, image });
+         io.to(usuarios[socket.username]).emit('send-private-message', { from: socket.username, message, image });
+      }else {
+         socket.emit('send-private-message-issue');
+      }
+   });
+
+   socket.on('disconnect', () => {
+      delete usuarios[socket.username];
+      io.emit('user-connected', usuarios);
+   }); 
+});
+
+
+
---
+++ src/main.js
@@ -0,0 +1,99 @@
+//HTML SCREENS
+const loginScreen = document.querySelector('.login-screen');
+const chatScreen = document.querySelector('.chat-screen');
+const conectedUsersArea = document.querySelector('.conectedUsersArea');
+chatScreen.style.display = 'none';
+conectedUsersArea.style.display = 'none';
+
+//HTML REFERENCES
+const usernameInput = document.querySelector('.usernameInput');
+const usernameButton = document.querySelector('.usernameButton');
+const inputMessage = document.querySelector('.inputMessage');
+const inputMessageButton = document.querySelector('.inputMessageButton');
+const messagesList = document.querySelector('.messages');
+const conectedUsers = document.querySelector('.conectedUsers');
+const photoInput = document.querySelector('.photoInput');
+const photoButton = document.querySelector('.photoButton');
+ 
+
+let DataURL;
+
+const socket = io();
+
+socket.on('user-connected', ( users ) => {
+    conectedUsers.innerHTML = '';
+    for (const user in users) {
+        conectedUsers.insertAdjacentHTML('beforeend',`<li>${user}</li>`);
+    }
+})
+
+socket.on('login', ( ) => {
+    loginScreen.style.display = 'none';
+    chatScreen.style.display = 'inline';
+    conectedUsersArea.style.display = 'inline';
+})
+
+socket.on('login-issue', () => {
+    alert('El nombre que intentas usar no esta disponible, intenta uno nuevo');
+    usernameInput.value = '';
+})
+
+socket.on('send-message', ({message, user, image}) => {
+    messagesList.insertAdjacentHTML('beforeend',`<li>${user}: ${message}</li>`);
+    if(image!== undefined){
+        const imagen = document.createElement("img")
+        imagen.src = image
+        messagesList.appendChild(imagen);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('send-private-message', ({from, message, image}) => {
+    messagesList.insertAdjacentHTML('beforeend',`<li>[Private Message] ${from}: ${message}</li>`);
+    if(image!== undefined){
+        const imagen = document.createElement("img")
+        imagen.src = image
+        const image  = $("<img>").css({
+            width : "50%",
+            height : "50%"
+        })
+        messagesList.appendChild(imagen);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('send-private-message-issue', () => {
+    alert('El usuario al que intentas enviar el mensaje no existe!');
+    usernameInput.value = '';
+})
+
+usernameButton.addEventListener('click', ()=>{
+    let username = usernameInput.value;
+    socket.emit('register', username);
+});
+
+inputMessageButton.addEventListener('click', ()=>{
+    if ( inputMessage.value.startsWith('/private') ) {
+        const targetUser = inputMessage.value.split(' ')[1];
+        const message = inputMessage.value.substr(targetUser.length + 9);
+        socket.emit('send-private-message', { targetUser, message, image: DataURL });
+    }else {
+        socket.emit('send-message', { message: inputMessage.value, image: DataURL});
+    }
+    inputMessage.value = '';
+    DataURL = undefined;
+});
+
+photoButton.addEventListener('click', ()=>{
+    photoInput.click();
+})
+
+photoInput.addEventListener('change', (e)=>{
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        DataURL = reader.result
+    };
+    reader.readAsDataURL(file);
+    
+})
